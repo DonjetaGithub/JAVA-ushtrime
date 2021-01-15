66c99a8febce3de657a16513ffb6f6ab22290e97
DonjetaGithub <62430921+DonjetaGithub@users.noreply.github.com>
2021-01-15 20:13:23 +0100

    Add files via upload

---
+++ CheckPerson.java
@@ -0,0 +1,12 @@
+public class CheckPerson {
+    public static void main(String[] args) {
+        Person personi1 = new Person("Donjeta", "Kadrija");
+        Person personi2 = new Person("Shqiponje", "Sopaj");
+        Person personi3 = new Person("Sara", "Neziri");
+        Person personi4 = new Person("Mehreme", "Selimi");
+        System.out.println(personi1);
+        System.out.println(personi2);
+        System.out.println(personi3);
+        System.out.println(personi4);
+    }
+}
---
+++ Example.java
@@ -0,0 +1,10 @@
+public class Example implements Interface1, Interface2{
+    @Override
+    public boolean myMethodInterface1(){
+        return false;
+    }
+    @Override
+    public String myMethodInterface2(){
+        return "Brock Lesnar";
+    }
+}
